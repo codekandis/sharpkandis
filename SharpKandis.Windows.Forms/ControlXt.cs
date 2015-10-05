a9ca6c012c609a70fcd0b4a39d11c308bf69d549
Christian Ramelow <info@derprogrammierer.com>
2015-10-05 21:41:35 +0200

    Fixed the comments due missing return value explanations and missing hint to passed action arguments.

--- SharpKandis.Windows.Forms/ControlXt.cs
+++ SharpKandis.Windows.Forms/ControlXt.cs
@@ -11,7 +11,7 @@
     /// <summary>Invokes an action on a specified control.</summary>
     /// <param name="control">The control to invoke the action passed in the argument <i>action</i>.</param>
     /// <param name="action">The action to invoke on the control passed in the argument <i>control</i>.</param>
-    /// <returns>The return value of the action, <i>null</i> otherwise.</returns>
+    /// <returns>The return value of the action if the action returns one, <i>null</i> otherwise.</returns>
     public static object Invoke( this Control control, Action action )
     {
       object result = control.Invoke( action );
@@ -21,8 +21,8 @@
     /// <summary>Invokes an action on a specified control.</summary>
     /// <param name="control">The control to invoke the action passed in the argument <i>action</i>.</param>
     /// <param name="action">The action to invoke on the control passed in the argument <i>control</i>.</param>
-    /// <param name="arguments">The arguments of the action passed in the argument <i>action</i>.</param>
-    /// <returns>The return value of the action, <i>null</i> otherwise.</returns>
+    /// <param name="arguments">The arguments to be passed to the action passed in the argument <i>action</i>.</param>
+    /// <returns>The return value of the action if the action returns one, <i>null</i> otherwise.</returns>
     public static object Invoke( this Control control, Action action, params object[ ] arguments )
     {
       object result = control.Invoke( action, ( object ) arguments );
