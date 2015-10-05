3553d2685460faa8426fe645acd3789833de7e92
Christian Ramelow <info@derprogrammierer.com>
2015-10-05 20:52:36 +0200

    !Suspended This project is in development state due serious bugs.

--- SharpKandis.Net/Sockets/DisconnectFailedEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SharpKandis.Net.Sockets
-{
-  /// <summary>Represents an event handler delegate of the <i>DisconnectFailed</i> event.</summary>
-  /// <param name="sender">The object which raised the <i>DisconnectFailed</i> event.</param>
-  /// <param name="eventArguments">The event arguments of the <i>DisconnectFailed</i> event.</param>
-  public delegate void DisconnectFailedEventHandler( object sender, DisconnectFailedEventArguments eventArguments );
-}
--- SharpKandis.Net/Sockets/ResponseSucceedEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SharpKandis.Net.Sockets
-{
-  /// <summary>Represents an event handler delegate of the <i>ResponseSucceed</i> event.</summary>
-  /// <param name="sender">The object which raised the <i>ResponseSucceed</i> event.</param>
-  /// <param name="eventArguments">The event arguments of the <i>ResponseSucceed</i> event.</param>
-  public delegate void ResponseSucceedEventHandler( object sender, ResponseSucceedEventArguments eventArguments );
-}
