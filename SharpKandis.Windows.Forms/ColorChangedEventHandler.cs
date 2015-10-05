828e933e76f1943ef290e1a8f51a2b40f7404cb5
Christian Ramelow <info@derprogrammierer.com>
2015-10-05 21:37:12 +0200

    Fixed the comment due unclear plural usage.

--- SharpKandis.Windows.Forms/ColorChangedEventHandler.cs
+++ SharpKandis.Windows.Forms/ColorChangedEventHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace SharpKandis.Windows.Forms
 {
-  /// <summary>Represents the event handler of all <i>ColorChanged</i> event.</summary>
+  /// <summary>Represents the event handler delegate of the <i>ColorChanged</i> event.</summary>
   /// <param name="sender">The object which raised the <i>ColorChanged</i> event.</param>
   /// <param name="eventArguments">The event arguments of the <i>ColorChanged</i> event.</param>
   public delegate void ColorChangedEventHandler( object sender, ColorChangedEventArguments eventArguments );
