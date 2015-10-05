ecafdc9dca5393c0a3ff9ac890085934c0cfe446
Christian Ramelow <info@derprogrammierer.com>
2015-10-05 12:14:37 +0200

    Added interface 'CountableInterface'. Represents the interface of all objects with countable elements.

---
+++ SharpKandis.Collections/CountableInterface.cs
@@ -0,0 +1,22 @@
+﻿namespace SharpKandis.Collections
+{
+  using System;
+
+  /// <summary>Represents the interface of all objects with countable elements.</summary>
+  public interface CountableInterface
+  {
+    #region Properties
+
+    #region Count
+
+    /// <summary>Gets the count of elements of the object.</summary>
+    int Count
+    {
+      get;
+    }
+
+    #endregion Count
+
+    #endregion Properties
+  }
+}
