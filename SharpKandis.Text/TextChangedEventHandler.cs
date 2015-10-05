3abe549dfcbd14359a228a8ce7cbb6d3fc55d46b
Christian Ramelow <info@derprogrammierer.com>
2015-10-05 19:54:02 +0200

    Fixed the comment due unclear plural usage.

--- SharpKandis.Text/TextChangedEventHandler.cs
+++ SharpKandis.Text/TextChangedEventHandler.cs
@@ -1,6 +1,6 @@
 ﻿namespace SharpKandis.Text
 {
-  /// <summary>Represents the event handler delegate of all <i>TextChanged</i> event.</summary>
+  /// <summary>Represents the event handler delegate of the <i>TextChanged</i> event.</summary>
   /// <param name="sender">The object which raised the <i>TextChanged</i> event.</param>
   /// <param name="eventArguments">The event arguments of the <i>TextChanged</i> event.</param>
   public delegate void TextChangedEventHandler( object sender, TextChangedEventArguments eventArguments );
