189c560801971c21f43e46e784cc8b8c50cae2cf
Christian Ramelow <info@dev.derprogrammierer.com>
2015-11-22 13:43:37 +0100

    Added the delegate 'DirectoryAccessEventHandler' and the class 'DirectoryAccessEventArguments' to provide directory access events.

---
+++ SharpKandis.Io/DirectoryAccessEventHandler.cs
@@ -0,0 +1,7 @@
+﻿namespace SharpKandis.Io
+{
+  /// <summary>Represents the event handler of all directory access events.</summary>
+  /// <param name="sender">The object which raised the event.</param>
+  /// <param name="eventArguments">The event arguments of the raised event.</param>
+  public delegate void DirectoryAccessEventHandler( object sender, DirectoryAccessEventArguments eventArguments );
+}
