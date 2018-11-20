b11cc037bbc88186883898dd6ae6b14420cd273d
Christian Ramelow <info@codekandis.net>
2018-11-20 20:26:05 +0100

    Adds a wrapped `decimal` value type.

---
+++ SharpKandis/DecimalType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `decimal` value type.
+	/// </summary>
+	public class DecimalType:
+		WrappedValueTypeAbstract<decimal>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public DecimalType( decimal value )
+			: base( value )
+		{
+		}
+	}
+}
