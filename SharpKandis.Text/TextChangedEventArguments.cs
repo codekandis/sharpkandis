c83c643b976c998d581766b9d5fdbd28c35a5a7d
Christian Ramelow <info@codekandis.net>
2018-11-20 13:10:02 +0100

    Reformats the project `SharpKandis.Text`.

--- SharpKandis.Text/TextChangedEventArguments.cs
+++ SharpKandis.Text/TextChangedEventArguments.cs
@@ -1,44 +1,32 @@
-﻿namespace SharpKandis.Text
+﻿using System;
+
+namespace SharpKandis.Text
 {
-  using System;
+	/// <summary>Represents the event arguments of the <i>TextChanged</i> event.</summary>
+	public class TextChangedEventArguments:
+		EventArgs
+	{
+		/// <summary>Stores the text that has been changed.</summary>
+		private string _text = string.Empty;
 
-  /// <summary>Represents the event arguments of the <i>TextChanged</i> event.</summary>
-  public class TextChangedEventArguments:
-    EventArgs
-  {
-    #region Properties
+		/// <summary>Gets / sets the text that has been changed.</summary>
+		public virtual string Text
+		{
+			get
+			{
+				return this._text;
+			}
+			private set
+			{
+				this._text = value;
+			}
+		}
 
-    #region Text
-
-    /// <summary>Stores the text that has been changed.</summary>
-    private string _text = string.Empty;
-
-    /// <summary>Gets / sets the text that has been changed.</summary>
-    public virtual string Text
-    {
-      get
-      {
-        return this._text;
-      }
-      private set
-      {
-        this._text = value;
-      }
-    }
-
-    #endregion Text
-
-    #endregion Properties
-
-    #region Constructors
-
-    /// <summary>Constructor method.</summary>
-    /// <param name="text">The text that has been changed.</param>
-    public TextChangedEventArguments( string text )
-    {
-      this.Text = text;
-    }
-
-    #endregion Constructors
-  }
+		/// <summary>Constructor method.</summary>
+		/// <param name="text">The text that has been changed.</param>
+		public TextChangedEventArguments( string text )
+		{
+			this.Text = text;
+		}
+	}
 }
--- SharpKandis.Text/TextChangedEventHandler.cs
+++ SharpKandis.Text/TextChangedEventHandler.cs
@@ -1,7 +1,7 @@
 ﻿namespace SharpKandis.Text
 {
-  /// <summary>Represents the event handler delegate of the <i>TextChanged</i> event.</summary>
-  /// <param name="sender">The object which raised the <i>TextChanged</i> event.</param>
-  /// <param name="eventArguments">The event arguments of the <i>TextChanged</i> event.</param>
-  public delegate void TextChangedEventHandler( object sender, TextChangedEventArguments eventArguments );
+	/// <summary>Represents the event handler delegate of the <i>TextChanged</i> event.</summary>
+	/// <param name="sender">The object which raised the <i>TextChanged</i> event.</param>
+	/// <param name="eventArguments">The event arguments of the <i>TextChanged</i> event.</param>
+	public delegate void TextChangedEventHandler( object sender, TextChangedEventArguments eventArguments );
 }
