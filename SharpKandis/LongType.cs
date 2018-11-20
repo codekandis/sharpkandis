e1e363c842decf4476c906e49c1be09c7c21404d
Christian Ramelow <info@codekandis.net>
2018-11-20 22:44:01 +0100

    Adds a wrapped `long` value type.

---
+++ SharpKandis/LongType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `long` value type.
+	/// </summary>
+	public class LongType:
+		WrappedValueTypeAbstract<long>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public LongType( long value )
+			: base( value )
+		{
+		}
+	}
+}
