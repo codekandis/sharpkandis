d8218cddfe9df2ee78a146de52636b66dcdca5a4
Christian Ramelow <info@codekandis.net>
2018-11-20 13:25:36 +0100

    Reformats the project `SharpKandis.Threading`.

--- SharpKandis.Threading/ThreadHelper.cs
+++ SharpKandis.Threading/ThreadHelper.cs
@@ -1,32 +1,28 @@
-﻿namespace SharpKandis.Threading
+﻿using System.Threading;
+
+namespace SharpKandis.Threading
 {
-  using System.Threading;
+	/// <summary>Represents an helper class for all threading purposes.</summary>
+	public class ThreadHelper
+	{
+		/// <summary>Starts a thread directly without the creation of a <i>System.Threading.ThreadStart</i> and a <i>System.Threading.Thread</i> instance.</summary>
+		/// <param name="threadStart">The delegate to be invoked while the thread starts.</param>
+		/// <returns>The started thread.</returns>
+		public static Thread StartDirect( ThreadStart threadStart )
+		{
+			Thread thread = new Thread( threadStart );
+			thread.Start( );
+			return thread;
+		}
 
-  /// <summary>Represents an helper class for all threading purposes.</summary>
-  public class ThreadHelper
-  {
-    #region Methods
-
-    /// <summary>Starts a thread directly without the creation of a <i>System.Threading.ThreadStart</i> and a <i>System.Threading.Thread</i> instance.</summary>
-    /// <param name="threadStart">The delegate to be invoked while the thread starts.</param>
-    /// <returns>The started thread.</returns>
-    public static Thread StartDirect( ThreadStart threadStart )
-    {
-      Thread thread = new Thread( threadStart );
-      thread.Start( );
-      return thread;
-    }
-
-    /// <summary>Starts a thread directly without the creation of a <i>System.Threading.ThreadStart</i> and a <i>System.Threading.Thread</i> instance.</summary>
-    /// <param name="parameterizedThreadStart">The delegate to be invoked while the thread starts.</param>
-    /// <returns>The started thread.</returns>
-    public static Thread StartDirect( ParameterizedThreadStart parameterizedThreadStart )
-    {
-      Thread thread = new Thread( parameterizedThreadStart );
-      thread.Start( );
-      return thread;
-    }
-
-    #endregion Methods
-  }
+		/// <summary>Starts a thread directly without the creation of a <i>System.Threading.ThreadStart</i> and a <i>System.Threading.Thread</i> instance.</summary>
+		/// <param name="parameterizedThreadStart">The delegate to be invoked while the thread starts.</param>
+		/// <returns>The started thread.</returns>
+		public static Thread StartDirect( ParameterizedThreadStart parameterizedThreadStart )
+		{
+			Thread thread = new Thread( parameterizedThreadStart );
+			thread.Start( );
+			return thread;
+		}
+	}
 }
