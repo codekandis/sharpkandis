b306b4a5f344a0cfd011b9c4c6bc9a2a9475c431
Christian Ramelow <info@codekandis.net>
2018-11-20 13:00:06 +0100

    Reformats the project `SharpKandis.Collections`.

--- SharpKandis.Collections/CountableInterface.cs
+++ SharpKandis.Collections/CountableInterface.cs
@@ -1,22 +1,12 @@
 ﻿namespace SharpKandis.Collections
 {
-  using System;
-
-  /// <summary>Represents the interface of all objects with countable elements.</summary>
-  public interface CountableInterface
-  {
-    #region Properties
-
-    #region Count
-
-    /// <summary>Gets the count of elements of the object.</summary>
-    int Count
-    {
-      get;
-    }
-
-    #endregion Count
-
-    #endregion Properties
-  }
+	/// <summary>Represents the interface of all objects with countable elements.</summary>
+	public interface CountableInterface
+	{
+		/// <summary>Gets the count of elements of the object.</summary>
+		int Count
+		{
+			get;
+		}
+	}
 }
