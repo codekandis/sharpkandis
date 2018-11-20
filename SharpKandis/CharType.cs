16680a9736b48f79e0498628e2057894140289fd
Christian Ramelow <info@codekandis.net>
2018-11-20 21:15:57 +0100

    Adds a wrapped `char` value type.

---
+++ SharpKandis/CharType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `char` value type.
+	/// </summary>
+	public class CharType:
+		WrappedValueTypeAbstract<char>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public CharType( char value )
+			: base( value )
+		{
+		}
+	}
+}
