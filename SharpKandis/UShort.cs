c7c087eb81c64e684267e6f9f7061c09ed5b4141
Christian Ramelow <info@codekandis.net>
2018-11-20 22:51:03 +0100

    Adds a wrapped `ushort` value type.

---
+++ SharpKandis/UShort.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `ushort` value type.
+	/// </summary>
+	public class UShortType:
+		WrappedValueTypeAbstract<ushort>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public UShortType( ushort value )
+			: base( value )
+		{
+		}
+	}
+}
