8fcde5b45cf4cd8b09780e12952e55f1b5399f4a
Christian Ramelow <info@codekandis.net>
2018-11-20 15:47:16 +0100

    Adds the interface of all wrapped value types.

---
+++ SharpKandis/WrappedValueTypeInterface.cs
@@ -0,0 +1,17 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents the interface of all wrapped value types.
+	/// </summary>
+	/// <typeparam name="TWrappedValueType">Specifies the wrapped type.</typeparam>
+	public interface WrappedValueTypeInterface<TWrappedValueType>
+	{
+		/// <summary>
+		/// Gets the value of the wrapped value type.
+		/// </summary>
+		TWrappedValueType Value
+		{
+			get;
+		}
+	}
+}
