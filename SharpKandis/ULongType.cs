25051207fc0e5e14716bb085819e8b572157fa8c
Christian Ramelow <info@codekandis.net>
2018-11-20 22:49:52 +0100

    Adds a wrapped `ulong` value type.

---
+++ SharpKandis/ULongType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `ulong` value type.
+	/// </summary>
+	public class ULongType:
+		WrappedValueTypeAbstract<ulong>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public ULongType( ulong value )
+			: base( value )
+		{
+		}
+	}
+}
