6e02b8d9faf3d5e91bfe854dfe8f48b36022a6b1
Christian Ramelow <info@codekandis.net>
2018-11-20 22:42:29 +0100

    Adds a wrapped `int` value type.

---
+++ SharpKandis/IntType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `int` value type.
+	/// </summary>
+	public class IntType:
+		WrappedValueTypeAbstract<int>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public IntType( int value )
+			: base( value )
+		{
+		}
+	}
+}
