609797212a48388a77048b74f912c0dfca3e5c20
Christian Ramelow <info@codekandis.net>
2018-11-20 22:47:01 +0100

    Adds a wrapped `short` value type.

---
+++ SharpKandis/ShortType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `short` value type.
+	/// </summary>
+	public class ShortType:
+		WrappedValueTypeAbstract<short>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public ShortType( short value )
+			: base( value )
+		{
+		}
+	}
+}
