9555a268d192c79f12e9c7a80ad96567f531055f
Christian Ramelow <info@codekandis.net>
2018-11-20 15:53:59 +0100

    Adds a wrapped `bool` value type.

---
+++ SharpKandis/BoolType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `bool` value type.
+	/// </summary>
+	public class BoolType:
+		WrappedValueTypeAbstract<bool>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public BoolType( bool value )
+			: base( value )
+		{
+		}
+	}
+}
---
+++ SharpKandis/DateTimeType.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `DateTime` value type.
+	/// </summary>
+	public class DateTimeType:
+		WrappedValueTypeAbstract<DateTime>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public DateTimeType( DateTime value )
+			: base( value )
+		{
+		}
+
+		/// <summary>
+		/// Defines the explicit type conversion from a `DateTime` value to a `DateTimeType`.
+		/// </summary>
+		/// <param name="value">The `DateTime` value to convert.</param>
+		public static explicit operator DateTimeType( DateTime value )
+		{
+			DateTimeType wrappedValueType = new DateTimeType( value );
+			return wrappedValueType;
+		}
+
+		/// <summary>
+		/// Defines the explicit type conversion from a `DateTimeType` to a `DateTime` value.
+		/// </summary>
+		/// <param name="value">The `DateTimeType` to convert.</param>
+		public static implicit operator DateTime( DateTimeType wrappedValueType )
+		{
+			return wrappedValueType.Value;
+		}
+	}
+}
