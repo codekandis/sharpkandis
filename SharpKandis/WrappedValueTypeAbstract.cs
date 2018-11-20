721aed4514325fff4aec40c06050b8f042704ec4
Christian Ramelow <info@codekandis.net>
2018-11-20 15:48:58 +0100

    Adds the base class of all wrapped value types.

---
+++ SharpKandis/WrappedValueTypeAbstract.cs
@@ -0,0 +1,39 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents the base class of all wrappe value types.
+	/// </summary>
+	/// <typeparam name="TWrappedValueType">Specifies the wrapped type.</typeparam>
+	public abstract class WrappedValueTypeAbstract<TWrappedValueType>:
+		WrappedValueTypeInterface<TWrappedValueType>
+	{
+		/// <summary>
+		/// Stores the value of the wrapped value type.
+		/// </summary>
+		private TWrappedValueType value;
+
+		/// <summary>
+		/// Gets / sets the value of the wrapped value type.
+		/// </summary>
+		public virtual TWrappedValueType Value
+		{
+			get
+			{
+				return this.value;
+			}
+			private set
+			{
+				this.value = value;
+			}
+		}
+
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public WrappedValueTypeAbstract( TWrappedValueType value )
+		{
+			this.Value = value;
+		}
+	}
+}
