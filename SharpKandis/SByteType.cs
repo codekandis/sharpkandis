719de710275020c7d4fadd9be24778a04cf7239b
Christian Ramelow <info@codekandis.net>
2018-11-20 22:46:03 +0100

    Adds a wrapped `sbyte` value type.

---
+++ SharpKandis/SByteType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `sbyte` value type.
+	/// </summary>
+	public class SByteType:
+		WrappedValueTypeAbstract<sbyte>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public SByteType( sbyte value )
+			: base( value )
+		{
+		}
+	}
+}
