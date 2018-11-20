d187899c76d2e5842650e58d2c512f559534b4c3
Christian Ramelow <info@codekandis.net>
2018-11-20 22:39:53 +0100

    Adds a wrapped `float` value type.

---
+++ SharpKandis/FloatType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `float` value type.
+	/// </summary>
+	public class FloatType:
+		WrappedValueTypeAbstract<float>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public FloatType( float value )
+			: base( value )
+		{
+		}
+	}
+}
