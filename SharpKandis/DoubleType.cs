50c57fbb13755083db173e4f6ac7b4c821b552f7
Christian Ramelow <info@codekandis.net>
2018-11-20 20:28:43 +0100

    Adds a wrapped `double` value type.

---
+++ SharpKandis/DoubleType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `double` value type.
+	/// </summary>
+	public class DoubleType:
+		WrappedValueTypeAbstract<double>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public DoubleType( double value )
+			: base( value )
+		{
+		}
+	}
+}
