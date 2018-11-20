be0aeaabd8c7ada062bf4c2afeb71518a7d414a5
Christian Ramelow <info@codekandis.net>
2018-11-20 22:49:15 +0100

    Adds a wrapped `uint` value type.

---
+++ SharpKandis/UIntType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `uint` value type.
+	/// </summary>
+	public class UIntType:
+		WrappedValueTypeAbstract<uint>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public UIntType( uint value )
+			: base( value )
+		{
+		}
+	}
+}
