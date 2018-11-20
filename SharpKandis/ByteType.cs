5c599c927999fa79503037e04f266bca9eb9850e
Christian Ramelow <info@codekandis.net>
2018-11-20 21:14:50 +0100

    Adds a wrapped `byte` value type.

---
+++ SharpKandis/ByteType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `byte` value type.
+	/// </summary>
+	public class ByteType:
+		WrappedValueTypeAbstract<byte>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public ByteType( byte value )
+			: base( value )
+		{
+		}
+	}
+}
