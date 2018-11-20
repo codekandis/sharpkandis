3e0ad877befe59a450ce9e7f0210205aa24f44ae
Christian Ramelow <info@codekandis.net>
2018-11-20 22:47:44 +0100

    Adds a wrapped `string` value type.

---
+++ SharpKandis/StringType.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpKandis
+{
+	/// <summary>
+	/// Represents a wrapped `string` value type.
+	/// </summary>
+	public class StringType:
+		WrappedValueTypeAbstract<string>
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped value.</param>
+		public StringType( string value )
+			: base( value )
+		{
+		}
+	}
+}
