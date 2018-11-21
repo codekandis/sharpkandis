e2587b82020b30da394ca8c8c3710d7802b8789e
Christian Ramelow <info@codekandis.net>
2018-11-21 13:58:21 +0100

    Adds the interface of all streamable classes.

---
+++ SharpKandis/Io/StreamableInterface.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace SharpKandis.Io
+{
+	/// <summary>
+	/// Represents the interface of all streamable classes.
+	/// </summary>
+	public interface StreamableInterface
+	{
+		/// <summary>
+		/// Creates the stream of the object.
+		/// </summary>
+		/// <returns>The stream of the object.</returns>
+		Stream ToStream( );
+	}
+}
