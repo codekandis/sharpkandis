64fb62acba0c0b360556d732dba7a749fd9f073f
Christian Ramelow <info@codekandis.net>
2018-11-21 15:17:55 +0100

    Adds the interface of all encrypting actions in an encryption context.

---
+++ SharpKandis/Security/Encryption/EncryptingActionInterface.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace SharpKandis.Security.Encryption
+{
+	/// <summary>
+	/// Represents the interface of all encrypting actions.
+	/// </summary>
+	public interface EncryptingActionInterface
+	{
+		/// <summary>
+		/// Encrypts a message.
+		/// </summary>
+		/// <param name="message">The message to encrypt.</param>
+		/// <returns>The encrypted message.</returns>
+		EncryptedMessageInterface Encrypt( MessageInterface message );
+
+		/// <summary>
+		/// Encrypts a stream with a message.
+		/// </summary>
+		/// <param name="inputStream">The input stream with the message to encrypt.</param>
+		/// <param name="outputStream">The output stream with the encrypted message.</param>
+		void Encrypt( Stream inputStream, Stream outputStream );
+	}
+}
