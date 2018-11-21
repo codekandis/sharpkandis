0a0b22a016c3cb4835d47f5950c1efc3064ae133
Christian Ramelow <info@codekandis.net>
2018-11-21 18:18:13 +0100

    Adds the interface of all signing actions in an encryption context.

---
+++ SharpKandis/Security/Encryption/SigningActionInterface.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace SharpKandis.Security.Encryption
+{
+	/// <summary>
+	/// Represents the interface of all signing actions.
+	/// </summary>
+	public interface SigningActionInterface
+	{
+		/// <summary>
+		/// Signs a message.
+		/// </summary>
+		/// <param name="message">The message to sign.</param>
+		/// <returns>The signed message.</returns>
+		SignedMessageInterface Sign( MessageInterface message );
+
+		/// <summary>
+		/// Signs a stream with a message.
+		/// </summary>
+		/// <param name="inputStream">The input stream with the message to sign.</param>
+		/// <param name="outputStream">The output stream with the signed message.</param>
+		void Sign( Stream inputStream, Stream outputStream );
+	}
+}
