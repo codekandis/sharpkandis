7f43a9cd6971d97d3a356eec653720328374828e
Christian Ramelow <info@codekandis.net>
2018-11-21 15:07:39 +0100

    Adds the interface of all encrypted messages in an ecryption context.

---
+++ SharpKandis/Security/Encryption/EncryptedMessageInterface.cs
@@ -0,0 +1,10 @@
+﻿namespace SharpKandis.Security.Encryption
+{
+	/// <summary>
+	/// Represents the interface of all encrypted messages.
+	/// </summary>
+	public interface EncryptedMessageInterface:
+		WrappedValueTypeInterface<string>
+	{
+	}
+}
