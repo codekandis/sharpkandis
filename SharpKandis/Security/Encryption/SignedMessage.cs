ce1630fa7f7cfb2ceb423a5e5520f26fd923e0cb
Christian Ramelow <info@codekandis.net>
2018-11-21 14:34:04 +0100

    Adds a signed message class in an ecryption context.

---
+++ SharpKandis/Security/Encryption/SignedMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace SharpKandis.Security.Encryption
+{
+	/// <summary>
+	/// Represents a signed message.
+	/// </summary>
+	public class SignedMessage:
+		StringType,
+		SignedMessageInterface
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped signed message.</param>
+		public SignedMessage( string value )
+			: base( value )
+		{
+		}
+	}
+}
