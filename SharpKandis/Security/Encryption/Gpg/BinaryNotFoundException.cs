598973c748278a4a9ac01c90a05c18aed17a8ce2
Christian Ramelow <info@codekandis.net>
2018-11-23 11:17:56 +0100

    Adds an exception if the GPG binary cannot be found.

---
+++ SharpKandis/Security/Encryption/Gpg/BinaryNotFoundException.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace SharpKandis.Security.Encryption.Gpg
+{
+	/// <summary>
+	/// Represents an exception if the GPG binary cannot be found.
+	/// </summary>
+	public class BinaryNotFoundException:
+		FileNotFoundException
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		public BinaryNotFoundException( )
+			: base( )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		public BinaryNotFoundException( string message )
+			: base( message )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		/// <param name="innerException">The inner exception of the exception.</param>
+		public BinaryNotFoundException( string message, Exception innerException )
+			: base( message, innerException )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		/// <param name="binaryPath">The path of the GPG binary.</param>
+		public BinaryNotFoundException( string message, string binaryPath )
+			: base( message, binaryPath )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		/// <param name="binaryPath">The path of the GPG binary.</param>
+		/// <param name="innerException">The inner exception of the exception.</param>
+		public BinaryNotFoundException( string message, string binaryPath, Exception innerException )
+			: base( message, binaryPath, innerException )
+		{
+		}
+	}
+}
