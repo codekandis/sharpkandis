0f8b496ae26fdc9e0334c2d04d41892c7c00bd90
Christian Ramelow <info@codekandis.net>
2018-11-23 07:16:03 +0100

    Adds the base class of all encryption provider actions.

---
+++ SharpKandis/Security/Encryption/EncryptionProviderActionAbstract.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpKandis.Security.Encryption
+{
+	/// <summary>
+	/// Represents the base class of all encryption provider actions.
+	/// </summary>
+	public abstract class EncryptionProviderActionAbstract
+	{
+		/// <summary>
+		/// Stores the encryption provider builder of the underlying encryption provider.
+		/// </summary>
+		private EncryptionProviderBuilderInterface encryptionProviderBuilder;
+
+		/// <summary>
+		/// Gets / sets the encryption provider builder of the underlying encryption provider.
+		/// </summary>
+		protected virtual EncryptionProviderBuilderInterface EncryptionProviderBuilder
+		{
+			get
+			{
+				return this.encryptionProviderBuilder;
+			}
+			private set
+			{
+				this.encryptionProviderBuilder = value;
+			}
+		}
+
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="encryptionProviderBuilder">The encryption provider builder of the underlying encryption provider.</param>
+		public EncryptionProviderActionAbstract( EncryptionProviderBuilderInterface encryptionProviderBuilder )
+		{
+			this.EncryptionProviderBuilder = encryptionProviderBuilder;
+		}
+	}
+}
