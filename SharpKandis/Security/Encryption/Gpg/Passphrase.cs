da12df0e42439e0dfb8130fa6a595082b4146ab6
Christian Ramelow <info@codekandis.net>
2018-11-23 11:08:46 +0100

    Adds a value type representing a passphrase of the user's GPG private key.

---
+++ SharpKandis/Security/Encryption/Gpg/Passphrase.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpKandis.Security.Encryption.Gpg
+{
+	/// <summary>
+	/// Represents a passphrase of the users GPG private key.
+	/// </summary>
+	public class Passphrase:
+		StringType
+	{
+		/// <summary>
+		/// Conststructor method.
+		/// </summary>
+		/// <param name="value">The initial passphrase of the users GPG private key.</param>
+		public Passphrase( string value )
+			: base( value )
+		{
+		}
+
+		/// <summary>
+		/// Defines the explicit type conversion from a `string` value to a `Passphrase`.
+		/// </summary>
+		/// <param name="value">The `string` value to convert.</param>
+		public static explicit operator Passphrase( string value )
+		{
+			Passphrase wrappedValueType = new Passphrase( value );
+			return wrappedValueType;
+		}
+
+		/// <summary>
+		/// Defines the explicit type conversion from a `Passphrase` to a `string` value.
+		/// </summary>
+		/// <param name="value">The `Passphrase` to convert.</param>
+		public static implicit operator string( Passphrase wrappedValueType )
+		{
+			return wrappedValueType.Value;
+		}
+	}
+}
