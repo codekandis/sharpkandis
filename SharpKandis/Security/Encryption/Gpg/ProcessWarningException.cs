70973a26786f20f4e51bc998475d6d55462b6181
Christian Ramelow <info@codekandis.net>
2018-11-23 11:34:51 +0100

    Adds an exception if a process warning of the GPG binary has been occured.

---
+++ SharpKandis/Security/Encryption/Gpg/ProcessWarningException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SharpKandis.Security.Encryption.Gpg
+{
+	/// <summary>
+	/// Represents an exception if a process warning of the GPG binary has been occured.
+	/// </summary>
+	public class ProcessWarningException:
+		Exception
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		public ProcessWarningException( )
+			: base( )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		public ProcessWarningException( string message )
+			: base( message )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		/// <param name="innerException">The inner exception of the exception.</param>
+		public ProcessWarningException( string message, Exception innerException )
+			: base( message, innerException )
+		{
+		}
+	}
+}
