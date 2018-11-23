a82a2a83e3d424f3c2ac8ce0edb7d607240cb073
Christian Ramelow <info@codekandis.net>
2018-11-23 11:42:10 +0100

    Adds a process standard error output warnings parser.

---
+++ SharpKandis/Security/Encryption/Gpg/ProcessWarningsParser.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SharpKandis.Security.Encryption.Gpg
+{
+	/// <summary>
+	/// Represents a process standard error output warnings parser. 
+	/// </summary>
+	public class ProcessWarningsParser:
+		ProcessStandardErrorOutputParserInterface
+	{
+		/// <summary>
+		/// Parses a line of the process's standard error output.
+		/// </summary>
+		/// <param name="line">The line to parse.</param>
+		public virtual void Parse( string line )
+		{
+			Match match = Regex.Match( line, @"^gpg: WARNING: (.+)$" );
+			if ( true == match.Success )
+			{
+				string exceptionMessage = match.Groups[ 1 ].Value;
+				throw new ProcessWarningException( exceptionMessage );
+			}
+		}
+	}
+}
