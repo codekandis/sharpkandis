4382d68f2ff38e612a6f1b50f6b93fa0695f6d1e
Christian Ramelow <info@codekandis.net>
2018-11-23 11:21:10 +0100

    Adds an exception if the recipient cannot be found in the key ring.

---
+++ SharpKandis/Security/Encryption/Gpg/RecipientNotFoundException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SharpKandis.Security.Encryption.Gpg
+{
+	/// <summary>
+	/// Represents an exception if the recipient cannot be found in the key ring.
+	/// </summary>
+	public class RecipientNotFoundException:
+		Exception
+	{
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		public RecipientNotFoundException( )
+			: base( )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		public RecipientNotFoundException( string message )
+			: base( message )
+		{
+		}
+
+		/// <summary>
+		/// Constructor method. 
+		/// </summary>
+		/// <param name="message">The message of the exception.</param>
+		/// <param name="innerException">The inner exception of the exception.</param>
+		public RecipientNotFoundException( string message, Exception innerException )
+			: base( message, innerException )
+		{
+		}
+	}
+}
