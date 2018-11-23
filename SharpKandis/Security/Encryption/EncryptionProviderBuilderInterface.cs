cff2dc709c421ccf3ad03cbac0a70a80be58760d
Christian Ramelow <info@codekandis.net>
2018-11-23 07:14:14 +0100

    Adds the interface of all encryption provider builders.

---
+++ SharpKandis/Security/Encryption/EncryptionProviderBuilderInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SharpKandis.Security.Encryption
+{
+	/// <summary>
+	/// Represents the interface of all encryption provider builders.
+	/// </summary>
+	public interface EncryptionProviderBuilderInterface
+	{
+		/// <summary>
+		/// Will be raised if a process error occures.
+		/// </summary>
+		event EventHandler<ProcessErrorEventArguments> ProcessError;
+
+		/// <summary>
+		/// Builds an encryption provider.
+		/// </summary>
+		/// <returns>The build encryption provider.</returns>
+		EncryptionProviderInterface BuildEncryptionProvider( );
+	}
+}
