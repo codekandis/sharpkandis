6f93690f7ae32f24c661fee33e0fae101458462b
Christian Ramelow <info@codekandis.net>
2018-11-23 11:14:11 +0100

    Adds a value type representing an ignore process errors flag.

---
+++ SharpKandis/Security/Encryption/Gpg/IgnoreProcessErrors.cs
@@ -0,0 +1,37 @@
+﻿namespace SharpKandis.Security.Encryption.Gpg
+{
+	/// <summary>
+	/// Represents an ignore process errors flag.
+	/// </summary>
+	public class IgnoreProcessErrors:
+		BoolType
+	{
+		/// <summary>
+		/// Conststructor method.
+		/// </summary>
+		/// <param name="value">The initial wrapped ignore process errors flag.</param>
+		public IgnoreProcessErrors( bool value )
+			: base( value )
+		{
+		}
+
+		/// <summary>
+		/// Defines the explicit type conversion from a `bool` value to an `IgnoreProcessErrors`.
+		/// </summary>
+		/// <param name="value">The `bool` value to convert.</param>
+		public static explicit operator IgnoreProcessErrors( bool value )
+		{
+			IgnoreProcessErrors wrappedValueType = new IgnoreProcessErrors( value );
+			return wrappedValueType;
+		}
+
+		/// <summary>
+		/// Defines the explicit type conversion from an `IgnoreProcessErrors` to a `bool` value.
+		/// </summary>
+		/// <param name="value">The `IgnoreProcessErrors` to convert.</param>
+		public static implicit operator bool( IgnoreProcessErrors wrappedValueType )
+		{
+			return wrappedValueType.Value;
+		}
+	}
+}
