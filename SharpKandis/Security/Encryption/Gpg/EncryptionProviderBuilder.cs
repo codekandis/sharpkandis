6437821419940a6ade47afa16dd5879eeb438415
Christian Ramelow <info@codekandis.net>
2018-11-23 11:52:06 +0100

    Adds a GPG encryption provider builder.

---
+++ SharpKandis/Security/Encryption/Gpg/EncryptionProviderBuilder.cs
@@ -0,0 +1,47 @@
+﻿namespace SharpKandis.Security.Encryption.Gpg
+{
+	/// <summary>
+	/// Represents a GPG encryption provider builder.
+	/// </summary>
+	public class EncryptionProviderBuilder:
+		EncryptionProviderBuilderInterface
+	{
+		/// <summary>
+		/// Stores the GPG encryption provider configuration.
+		/// </summary>
+		private EncryptionProviderConfigInterface encryptionProviderConfig;
+
+		/// <summary>
+		/// Gets / sets the GPG encryption provider configuration.
+		/// </summary>
+		private EncryptionProviderConfigInterface EncryptionProviderConfig
+		{
+			get
+			{
+				return this.encryptionProviderConfig;
+			}
+			set
+			{
+				this.encryptionProviderConfig = value;
+			}
+		}
+
+		/// <summary>
+		/// Constructor method.
+		/// </summary>
+		/// <param name="encryptionProviderConfig">The GPG encryption provider configuration.</param>
+		public EncryptionProviderBuilder( EncryptionProviderConfigInterface encryptionProviderConfig )
+		{
+			this.EncryptionProviderConfig = encryptionProviderConfig;
+		}
+
+		/// <summary>
+		/// Builds a GPG encryption provider.
+		/// </summary>
+		/// <returns>The build GPG encryption provider.</returns>
+		public virtual EncryptionProviderInterface BuildEncryptionProvider( )
+		{
+			return new EncryptionProvider( this.EncryptionProviderConfig );
+		}
+	}
+}
