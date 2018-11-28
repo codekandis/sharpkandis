ccc013b3fa9040d2f2dbae41a2fd80d837028caa
Christian Ramelow <info@codekandis.net>
2018-11-29 00:36:21 +0100

    Adds a wrapped `DateTime` value type.

--- SharpKandis/DateTimeType.cs
+++ SharpKandis/DateTimeType.cs
@@ -16,24 +16,5 @@ namespace SharpKandis
 			: base( value )
 		{
 		}
-
-		/// <summary>
-		/// Defines the explicit type conversion from a `DateTime` value to a `DateTimeType`.
-		/// </summary>
-		/// <param name="value">The `DateTime` value to convert.</param>
-		public static explicit operator DateTimeType( DateTime value )
-		{
-			DateTimeType wrappedValueType = new DateTimeType( value );
-			return wrappedValueType;
-		}
-
-		/// <summary>
-		/// Defines the explicit type conversion from a `DateTimeType` to a `DateTime` value.
-		/// </summary>
-		/// <param name="value">The `DateTimeType` to convert.</param>
-		public static implicit operator DateTime( DateTimeType wrappedValueType )
-		{
-			return wrappedValueType.Value;
-		}
 	}
 }
