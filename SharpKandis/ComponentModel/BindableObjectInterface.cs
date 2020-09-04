6ca7582fd85038e1f70d8008f8ec1f2ea9865cfb
Christian Ramelow <info@codekandis.net>
2020-09-04 14:49:24 +0200

    Adds the interface `BindableObjectInterface`.

---
+++ SharpKandis/ComponentModel/BindableObjectInterface.cs
@@ -0,0 +1,13 @@
+﻿namespace SharpKandis.ComponentModel
+{
+	/// <summary>Represents the interface of all bindable objects.</summary>
+	public interface BindableObjectInterface
+	{
+		/// <summary>Gets / sets the data source of the object.</summary>
+		object DataSource
+		{
+			get;
+			set;
+		}
+	}
+}
